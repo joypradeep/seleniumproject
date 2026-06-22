31528287e9dff67356be1ace4267823accbecc16
Joy Pradeep <joypradeep1995@gmail.com>
2026-06-22 12:04:08 +0530

    initial commit

---
+++ src/test/java/org/joy/pradeep/tests/GoogleTests.java
@@ -0,0 +1,17 @@
+package org.joy.pradeep.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class GoogleTests {
+    @Test
+    public void openGoogle(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+}
